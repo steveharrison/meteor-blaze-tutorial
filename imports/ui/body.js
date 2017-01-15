10d82363de3ef41ff13b736938be00f7f171f92e
Steve Harrison <steve.harrison.me@me.com>
2017-01-15 13:55:40 +1100

    Finished tutorial with security + testing improvements.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,6 +9,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
  
 Template.body.helpers({
@@ -39,12 +40,13 @@ Template.body.events({
     	const text = target.text.value;
  	
     	// Insert a task into the collection
-    	Tasks.insert({
-    	  text,
-    	  createdAt: new Date(), // current time,
-          owner: Meteor.userId(),
-      	  username: Meteor.user().username
-    	});
+      Meteor.call('tasks.insert', text);
+    	// Tasks.insert({
+    	//   text,
+    	//   createdAt: new Date(), // current time,
+     //      owner: Meteor.userId(),
+     //  	  username: Meteor.user().username
+    	// });
  	
     	// Clear form
     	target.text.value = '';
